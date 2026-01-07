ffab7f5c2f2fa0d60162c9b868e034ad875aac89
Ali Ghazanfar <124116007+ghazanfar18@users.noreply.github.com>
2026-01-07 23:11:32 +0500

    Add files via upload

---
+++ JunitLab10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.junitlab10;
+
+/**
+ *
+ * @author Ali
+ */
+public class JunitLab10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Junittesting11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.junittesting11;
+
+/**
+ *
+ * @author Ali
+ */
+public class Junittesting11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
